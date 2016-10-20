b58d43130033ea38057ddee057c079b126ed6235
nickidebruyn <nickidebruyn@gmail.com>
2016-10-20 13:22:21 +0200

    sdsdsdsd

--- scripts/quote-create.js
+++ scripts/quote-create.js
@@ -60,25 +60,14 @@
       console.log("Quote successfully saved: ", vm.quote);
 
 
-
       //Generate the js pdf
       var doc = new jsPDF();
 
-      // I know the proper spelling is colour ;)
-      doc.setTextColor(100);
-      doc.text(20, 20, 'This is gray.');
+      var docWidth = 800;
 
-      doc.setTextColor(150);
-      doc.text(20, 30, 'This is light gray.');
-
-      doc.setTextColor(255, 0, 0);
-      doc.text(20, 40, 'This is red.');
-
-      doc.setTextColor(0, 255, 0);
-      doc.text(20, 50, 'This is green.');
-
-      doc.setTextColor(0, 0, 255);
-      doc.text(20, 60, 'This is blue.');
+      doc.setFontSize(32);
+      doc.setTextColor(0);
+      doc.text(400, 20, 'EXCEPTIONAL SERVICE SINCE 1979');
 
       doc.save('Test.pdf');
 
